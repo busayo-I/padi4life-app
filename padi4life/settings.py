f4a6975a7acbf7167c2fb78926ebbf90caab9549
busayo <ibrahimbusay@gmail.com>
2023-10-29 15:46:59 +0100

    updating

--- padi4life/settings.py
+++ padi4life/settings.py
@@ -81,14 +81,21 @@ WSGI_APPLICATION = 'padi4life.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'padi4life',         
-        'USER': 'root',         
-        'PASSWORD': 'busayo08/07', 
-        'HOST': 'localhost',            
-        'PORT': '3306',                
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'padi4life',         
+#         'USER': 'root',         
+#         'PASSWORD': 'busayo08/07', 
+#         'HOST': 'localhost',            
+#         'PORT': '3306',                
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
